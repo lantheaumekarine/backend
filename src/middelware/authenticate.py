6537ac90fa711f917c79e844f5ab575884478d8c
Camille Barnier <brnrcamille@gmail.com>
2024-08-22 22:33:48 +0200

    remove passlib

--- src/middelware/authenticate.py
+++ src/middelware/authenticate.py
@@ -1,11 +1,6 @@
 from fastapi import Request
 import bcrypt
-from passlib.context import CryptContext
-from datetime import datetime, timedelta  
-from utils.config import SECRET_KEY, JWT_ALGORITHM, JWT_AUDIENCE, JWT_TOKEN_PREFIX, ACCESS_TOKEN_EXPIRE_MINUTES
-from data.user import UserPasswordUpdate
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 async def authenticate(request: Request, call_next):
         # TODO - cert auth : get fields passed by apache (https://stackoverflow.com/a/7691293) -> get user with infos
@@ -22,17 +17,17 @@ class AuthSrvice:
     
     
     def create_salt_and_hashed_password(self, plaintext_password: str):
-        salt = self.generate_salt()
+        salt = self.generate_salt().decode('utf-8')
         hashed_password = self.hash_password(password=plaintext_password, salt=salt)
         return salt, hashed_password
 
     def generate_salt(self):
-        return bcrypt.gensalt().decode()
+        return bcrypt.gensalt()
     
     def hash_password(self,*, password: str, salt: str):
-        return pwd_context.hash(password + salt)
+        return bcrypt.hashpw(password.encode(),salt.encode()).decode('utf-8')
 
     def verify_password(self, *, password: str, salt: str, hashed_pw: str):
-        return pwd_context.verify(password + salt, hashed_pw)
+        return bcrypt.checkpw(password.encode('utf-8'), hashed_pw.encode('utf-8'))
 
 
